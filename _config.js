60b62aeb881bdea14f3818a792c5a74eb7fbfa74
Felix <felixrunye@gmail.com>
2024-04-14 12:34:09 +0300

    added Jenkins file

--- _config.js
+++ _config.js
@@ -4,8 +4,7 @@ var config = {};
 config.mongoURI = {
   production:
     "mongodb+srv://felixrunye:QMOXJVAcFGrOdCnp@moringa-devops-cluster.tlrmpwq.mongodb.net/?retryWrites=true&w=majority&appName=moringa-devops-cluster",
-  development:
-    "mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority",
+  development: "mongodb://localhost:27017/gallery-devops", //"mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority",
   test: "mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority",
 };
 module.exports = config;
